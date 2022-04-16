044f04285c445d073df438dfb33475710ce345c0
Zhdanov-Vyacheslav <90984177+Zhdanov-Vyacheslav@users.noreply.github.com>
2022-04-16 15:27:08 +0300

    Lesson_4 Full

--- dz_1.js
+++ dz_1.js
@@ -3,27 +3,26 @@
 // Например, для числа 245 мы должны получить следующий объект: {‘единицы’: 5, ‘десятки’: 4, ‘сотни’: 2}. 
 // Если число превышает 999, необходимо выдать соответствующее сообщение с помощью console.log и вернуть пустой объект.
 
-const objNumber = {
-	thousands: 0,
-	hundreds: 0,
-	tens: 0
-};
-
 function convert(number){
+	const objNumber = {
+		'еденицы': 0,
+		'десятки': 0,
+		'сотни': 0
+	};
 	if (number > 999) {
-		console.log(number,' > 999 ')
-		return objNumber
+		console.log(number,' > 999 ');
+		return objNumber;
 	}
 	while (number != 0){
 		if (number < 10) {
-			objNumber.tens = number
-			number = 0
+			objNumber['еденицы'] = number;
+			number = 0;
 		} else if (number < 100){
-			objNumber.hundreds = (number / 10) | 0;
-			number = number % 10;
+			objNumber['десятки'] = (number / 10) | 0;
+			number %= 10;
 		} else if (number < 1000){
-			objNumber.thousands = (number / 100) | 0;
-			number = number % 100;
+			objNumber['сотни'] = (number / 100) | 0;
+			number %= 100;
 		}
 	}
 	return objNumber
--- dz_2.js
+++ dz_2.js
@@ -2,3 +2,40 @@
 // 2.1. В прошлом домашнем задании вы реализовали корзину на базе массивов. Какими объектами можно заменить их элементы?
 // 2.2. Реализуйте такие объекты.
 // 2.3. Перенести функционал подсчета корзины на объектно-ориентированную базу.
+
+// Шаблон продукта
+// const product = {
+// 	name : String,
+// 	price : Number,
+// }
+
+const basket = {
+	products : {'length':0},
+	basketPrice : 0,
+
+	getBasketPice(){
+		for (let i = 0; i != this.products.length; i++){
+			this.basketPrice += this.products[i].count * this.products[i].price
+		}
+		return this.basketPrice
+	},
+
+	putProduct(product, count){
+		product['count'] = count
+		// Повторное добавление имеющегося товара заменит имеющийся
+		for (let i = 0; i != this.products.length; i++){
+			if (this.products[i].name === product.name) {
+				this.products[i] = product;
+				return
+			};
+		};
+		this.products[this.products.length++] = product
+	},
+}
+
+console.log(basket.getBasketPice());
+basket.putProduct({'name':'apple', 'price':200}, 3)
+basket.putProduct({'name':'orange', 'price':100}, 1)
+basket.putProduct({'name':'apple', 'price':200}, 2)
+basket.putProduct({'name':'milk', 'price':150.50}, 3)
+console.log(basket.getBasketPice());
